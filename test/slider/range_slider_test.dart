76cbde7ab2bafd33b2f16582bdeb94e05855e76a
Udo Schöfer <developer@udos86.de>
2023-12-20 21:33:04 +0100

    updates tests

--- test/slider/range_slider_test.dart
+++ test/slider/range_slider_test.dart
@@ -4,24 +4,26 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../test_utils.dart';
 
+findFastRangeSlider() => find.byType(FastRangeSlider);
+
+findRangeSlider() => find.byType(RangeSlider);
+
 void main() {
   testWidgets('renders FastRangeSlider', (tester) async {
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastRangeSlider(
-        name: 'range_slider',
-        max: 10,
-        min: 0,
-        labelsBuilder: rangeSliderLabelsBuilder,
-        prefixBuilder: rangeSliderPrefixBuilder,
-        suffixBuilder: rangeSliderSuffixBuilder,
-      ),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(FastRangeSlider(
+      name: 'range_slider',
+      max: 10,
+      min: 0,
+      labelsBuilder: rangeSliderLabelsBuilder,
+      prefixBuilder: rangeSliderPrefixBuilder,
+      suffixBuilder: rangeSliderSuffixBuilder,
+    )));
 
-    final fastRangeSliderFinder = find.byType(FastRangeSlider);
-    final rangeSliderFinder = find.byType(RangeSlider);
+    final fastRangeSliderFinder = findFastRangeSlider();
 
     expect(fastRangeSliderFinder, findsOneWidget);
-    expect(rangeSliderFinder, findsOneWidget);
+    expect(findInputDecorator(), findsOneWidget);
+    expect(findRangeSlider(), findsOneWidget);
 
     final widget = tester.widget(fastRangeSliderFinder) as FastRangeSlider;
 
@@ -33,50 +35,56 @@ void main() {
     expect(suffixFinder, findsOneWidget);
   });
 
+  testWidgets('renders FastRangeSlider without InputDecorator', (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp(FastRangeSlider(
+      name: 'range_slider',
+      showInputDecoration: false,
+    )));
+
+    final fastRangeSliderFinder = findFastRangeSlider();
+
+    expect(fastRangeSliderFinder, findsOneWidget);
+    expect(findInputDecorator(), findsNothing);
+    expect(findRangeSlider(), findsOneWidget);
+  });
+
   testWidgets('updates FastRangeSlider', (tester) async {
     final spy = OnChangedSpy<RangeValues>();
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastRangeSlider(
-        name: 'range_slider',
-        max: 10,
-        min: 0,
-        prefixBuilder: rangeSliderPrefixBuilder,
-        suffixBuilder: rangeSliderSuffixBuilder,
-        onChanged: spy.fn,
-      ),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(FastRangeSlider(
+      name: 'range_slider',
+      max: 10,
+      min: 0,
+      prefixBuilder: rangeSliderPrefixBuilder,
+      suffixBuilder: rangeSliderSuffixBuilder,
+      onChanged: spy.fn,
+    )));
 
-    final state =
-        tester.state(find.byType(FastRangeSlider)) as FastRangeSliderState;
-    const testValues = RangeValues(5, 7);
+    final state = tester.state(findFastRangeSlider()) as FastRangeSliderState;
+    const updatedValues = RangeValues(5, 7);
 
-    state.didChange(testValues);
+    state.didChange(updatedValues);
     await tester.pumpAndSettle();
 
-    expect(spy.calledWith, testValues);
-    expect(find.text(testValues.start.toStringAsFixed(0)), findsOneWidget);
-    expect(find.text(testValues.end.toStringAsFixed(0)), findsOneWidget);
+    expect(spy.calledWith, updatedValues);
+    expect(find.text(updatedValues.start.toStringAsFixed(0)), findsOneWidget);
+    expect(find.text(updatedValues.end.toStringAsFixed(0)), findsOneWidget);
   });
 
   testWidgets('validates FastRangeSlider', (tester) async {
     const errorText = 'Range is too narrow';
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastRangeSlider(
-        name: 'range_slider',
-        max: 10,
-        min: 0,
-        validator: (values) =>
-            values!.end - values.start <= 1 ? errorText : null,
-      ),
-    ));
-
-    final state =
-        tester.state(find.byType(FastRangeSlider)) as FastRangeSliderState;
+    await tester.pumpWidget(buildMaterialTestApp(FastRangeSlider(
+      name: 'range_slider',
+      max: 10,
+      min: 0,
+      validator: (values) => values!.end - values.start <= 1 ? errorText : null,
+    )));
 
     final errorTextFinder = find.text(errorText);
     expect(errorTextFinder, findsNothing);
+
+    final state = tester.state(findFastRangeSlider()) as FastRangeSliderState;
 
     state.didChange(const RangeValues(8, 9));
     await tester.pumpAndSettle();
--- test/slider/slider_test.dart
+++ test/slider/slider_test.dart
@@ -6,22 +6,24 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../test_utils.dart';
 
-void main() {
-  testWidgets('renders Material FastSlider', (tester) async {
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastSlider(
-        name: 'slider',
-        labelBuilder: sliderLabelBuilder,
-        prefixBuilder: (state) => const Icon(Icons.volume_up),
-        suffixBuilder: sliderSuffixBuilder,
-      ),
-    ));
+findFastSlider() => find.byType(FastSlider);
 
-    final fastSliderFinder = find.byType(FastSlider);
-    final sliderFinder = find.byType(Slider);
+findSlider() => find.byType(Slider);
+
+void main() {
+  testWidgets('renders FastSlider', (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp(FastSlider(
+      name: 'slider',
+      labelBuilder: sliderLabelBuilder,
+      prefixBuilder: (state) => const Icon(Icons.volume_up),
+      suffixBuilder: sliderSuffixBuilder,
+    )));
+
+    final fastSliderFinder = findFastSlider();
 
     expect(fastSliderFinder, findsOneWidget);
-    expect(sliderFinder, findsOneWidget);
+    expect(findInputDecorator(), findsOneWidget);
+    expect(findSlider(), findsOneWidget);
 
     final widget = tester.widget(fastSliderFinder) as FastSlider;
 
@@ -32,18 +34,27 @@ void main() {
     expect(suffixFinder, findsOneWidget);
   });
 
-  testWidgets('renders Cupertino FastSlider', (tester) async {
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastSlider(
-        name: 'slider',
-        builder: cupertinoSliderBuilder,
-        labelBuilder: sliderLabelBuilder,
-        prefixBuilder: (state) => const Icon(Icons.volume_up),
-        suffixBuilder: sliderSuffixBuilder,
-      ),
-    ));
+  testWidgets('renders FastSlider without InputDecoration', (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp(const FastSlider(
+      name: 'slider',
+      showInputDecoration: false,
+    )));
 
-    final fastSliderFinder = find.byType(FastSlider);
+    expect(findFastSlider(), findsOneWidget);
+    expect(findInputDecorator(), findsNothing);
+    expect(findSlider(), findsOneWidget);
+  });
+
+  testWidgets('renders Cupertino FastSlider', (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp(FastSlider(
+      name: 'slider',
+      builder: cupertinoSliderBuilder,
+      labelBuilder: sliderLabelBuilder,
+      prefixBuilder: (state) => const Icon(Icons.volume_up),
+      suffixBuilder: sliderSuffixBuilder,
+    )));
+
+    final fastSliderFinder = findFastSlider();
     final sliderFinder = find.byType(CupertinoSlider);
 
     expect(fastSliderFinder, findsOneWidget);
@@ -61,15 +72,13 @@ void main() {
   testWidgets('updates FastSlider', (tester) async {
     final spy = OnChangedSpy<double>();
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastSlider(
-        name: 'slider',
-        suffixBuilder: sliderSuffixBuilder,
-        onChanged: spy.fn,
-      ),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(FastSlider(
+      name: 'slider',
+      suffixBuilder: sliderSuffixBuilder,
+      onChanged: spy.fn,
+    )));
 
-    final state = tester.state(find.byType(FastSlider)) as FastSliderState;
+    final state = tester.state(findFastSlider()) as FastSliderState;
     final testValue = state.widget.max;
 
     state.didChange(testValue);
@@ -82,14 +91,12 @@ void main() {
   testWidgets('validates FastSlider', (tester) async {
     const errorText = 'Value is too high';
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      FastSlider(
-        name: 'slider',
-        validator: (value) => value! > 0 ? errorText : null,
-      ),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(FastSlider(
+      name: 'slider',
+      validator: (value) => value! > 0 ? errorText : null,
+    )));
 
-    final state = tester.state(find.byType(FastSlider)) as FastSliderState;
+    final state = tester.state(findFastSlider()) as FastSliderState;
 
     final errorTextFinder = find.text(errorText);
     expect(errorTextFinder, findsNothing);
@@ -103,11 +110,12 @@ void main() {
   testWidgets('adapts FastSlider to Android', (tester) async {
     debugDefaultTargetPlatformOverride = TargetPlatform.android;
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      const FastSlider(name: 'slider', adaptive: true),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(const FastSlider(
+      name: 'slider',
+      adaptive: true,
+    )));
 
-    expect(find.byType(Slider), findsOneWidget);
+    expect(findSlider(), findsOneWidget);
 
     debugDefaultTargetPlatformOverride = null;
   });
@@ -115,9 +123,10 @@ void main() {
   testWidgets('adapts FastSlider to iOS', (tester) async {
     debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
-    await tester.pumpWidget(buildMaterialTestApp(
-      const FastSlider(name: 'slider', adaptive: true),
-    ));
+    await tester.pumpWidget(buildMaterialTestApp(const FastSlider(
+      name: 'slider',
+      adaptive: true,
+    )));
 
     expect(find.byType(CupertinoFormRow), findsOneWidget);
     expect(find.byType(CupertinoSlider), findsOneWidget);
