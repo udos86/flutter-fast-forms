b1620f4962821727a60555d6b25440e63725004d
Udo Schöfer <developer@udos86.de>
2024-06-02 00:03:35 +0200

    updates FastDateRangePicker tests

--- test/date_time/date_range_picker_test.dart
+++ test/date_time/date_range_picker_test.dart
@@ -24,22 +24,6 @@ void main() {
     await tester.pumpAndSettle();
   });
 
-  testWidgets('shows CalendarDateRangePicker on GestureDetector tap',
-      (tester) async {
-    await tester.pumpWidget(buildMaterialTestApp([
-      FastDateRangePicker(
-        name: 'date_picker',
-        firstDate: DateTime(1900),
-        lastDate: DateTime.now().add(const Duration(days: 365)),
-      ),
-    ]));
-
-    await tester.tap(findInkWell().first);
-    await tester.pumpAndSettle();
-
-    expect(find.byType(DateRangePickerDialog), findsOneWidget);
-  });
-
   testWidgets('updates FastDateRangerPicker', (tester) async {
     await tester.pumpWidget(buildMaterialTestApp([
       FastDateRangePicker(
@@ -61,5 +45,63 @@ void main() {
 
     final dateRangePickerText = dateRangPickerTextBuilder(state);
     expect(find.text(dateRangePickerText.data!), findsOneWidget);
+  });
+
+  testWidgets('shows input date range picker dialog on GestureDetector tap',
+      (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp([
+      FastDateRangePicker(
+        name: 'date_range_picker',
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now().add(const Duration(days: 365)),
+      ),
+    ]));
+
+    await tester.tap(findInkWell().first);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(DateRangePickerDialog), findsOneWidget);
+  });
+
+  testWidgets('shows calendar date range picker dialog on calendar button tap',
+      (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp([
+      FastDateRangePicker(
+        name: 'date_range_picker',
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now().add(const Duration(days: 365)),
+      ),
+    ]));
+
+    final calenderButton = find.widgetWithIcon(IconButton, Icons.today);
+    await tester.tap(calenderButton);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(DateRangePickerDialog), findsOneWidget);
+  });
+
+  testWidgets('updates state when touched', (tester) async {
+    await tester.pumpWidget(buildMaterialTestApp([
+      FastDateRangePicker(
+        name: 'date_range_picker',
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now().add(const Duration(days: 365)),
+      ),
+    ]));
+
+    final state =
+        tester.state<FastDateRangePickerState>(findFastDateRangePicker());
+
+    final calenderButton = find.widgetWithIcon(IconButton, Icons.today);
+    await tester.tap(calenderButton);
+    await tester.pumpAndSettle();
+
+    expect(state.touched, false);
+
+    final closeButton = find.widgetWithIcon(IconButton, Icons.close);
+    await tester.tap(closeButton);
+    await tester.pumpAndSettle();
+
+    expect(state.touched, true);
   });
 }
